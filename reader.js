130e43e9d4f23632cf841ea89e666989730c3682
disnotons <35609374+disnotons@users.noreply.github.com>
2026-08-22 11:41:48 +0900

    Use segmented reader for SA volume 26

--- reader.js
+++ reader.js
@@ -15,7 +15,8 @@ const SPLIT_VOLUMES={
   13:['13.seg1.mdpart','13.seg2.mdpart','13.seg3.mdpart'],
   14:['14.seg1.mdpart','14.seg2.mdpart','14.seg3.mdpart'],
   15:['15.seg1.mdpart','15.seg2.mdpart','15.seg3.mdpart'],
-  16:['16.seg1.mdpart','16.seg2.mdpart','16.seg3.mdpart']
+  16:['16.seg1.mdpart','16.seg2.mdpart','16.seg3.mdpart'],
+  26:['26.seg1.mdpart','26.seg2.mdpart','26.seg3.mdpart','26.seg4.mdpart']
 };
 
 function navHtml(){
